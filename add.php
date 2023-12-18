10346c783c586c18de9eefecb5bae39a6b88e999
DevAlejandroAF <alejandroavi95@hotmail.com>
2023-12-18 16:49:10 -0500

    data validation and binding

--- add.php
+++ add.php
@@ -2,12 +2,22 @@
 
 require "database.php";
 
+$error = null;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $phone_number = $_POST["phone_number"];
-  $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phone_number')");
-  $statement->execute();
-  header("Location: index.php");
+  if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
+    $error = "Please fill all fields.";
+  } else if (strlen($_POST["phone_number"]) < 7) {
+    $error = "Phone number must be at least 7 characters";
+  } else {
+    $name = $_POST["name"];
+    $phone_number = $_POST["phone_number"];
+    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+    $statement->bindParam(":name", $_POST["name"]);
+    $statement->bindParam(":phone_number", $_POST["phone_number"]);
+    $statement->execute();
+    header("Location: index.php");
+  }
 }
 ?>
 
@@ -53,6 +63,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php if ($error) : ?>
+                <p class="text-danger">
+                  <?= $error ?>
+                </p>
+              <?php endif ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
@@ -83,5 +98,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
   </main>
 </body>
+
+
+
+
+
+
 
 </html>
